2d6ff8aae816ecab0648de550a8e30c0e66eda63
SergeyTy <58304169+SergeyTy@users.noreply.github.com>
2020-12-07 17:08:35 +0700

    Add description

--- INF/INF 1/INF_Lab_1/Program.cs
+++ INF/INF 1/INF_Lab_1/Program.cs
@@ -19,13 +19,15 @@ namespace INF_Lab_1
         static void Main(string[] args)
         {
             Console.Write("Введите число от 127 до -128: ");
-            int a = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());//Чтение числа из консоли
             sbyte r = (sbyte)(a);
             
             //printM(SByteToBin(r));
             Console.WriteLine(binToStr(SByteToBin(r)));
             Console.ReadKey();
         }
+        
+        //Функция перевода массива в строку
         static string binToStr(int[] n)
         {
             string str;
@@ -34,6 +36,7 @@ namespace INF_Lab_1
 
             return str;
         }
+        //Функция вывода массива в консоль
         static void printM(int[] m)
         {
             for (int i = 0; i < m.Length; i++)
@@ -42,14 +45,14 @@ namespace INF_Lab_1
             }
             Console.WriteLine();
         }
+        //Функция перевода sbyte в int[]
         static int[] SByteToBin(sbyte n)
         {
             int[] m = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
             for (int i = 7; i >= 0; i--)
             {
-                //sbyte a = 1;//00000001
-                m[i] = n & 1;
-                n = (sbyte)(n >> 1);
+                m[i] = n & 1; //операция & с 1 накладывает маску на число sbyte и записывает разряд еденицы числа
+                n = (sbyte)(n >> 1); //Сдвиг вчисла на один разряд
             }
             return m;
         }
