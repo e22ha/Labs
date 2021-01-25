8464f2932d7e1d638f608cd86efea7854e955301
Sergey <seregio2002@outlook.com>
2021-01-25 14:26:07 +0700

    add dataBase

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,9 +21,26 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=D:\\Code\\LABs_2\\WpfApp1\\dataBase.sqlite;Version=3;");
+            //открытие соединения с базой данных
+            m_dbConnection.Open();
+            //выполнение запросов
+            //формирование запроса на добавление данных в поля типа INTEGER и TEXT
+            //обратите внимание, что в текстовое поле, данные добавляются в формате ‘data’
+            string sql = "INSERT INTO id_name (id, fio) VALUES (" + tb1.Text + ",'" + tb2.Text + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //извлечение запроса
+            command.ExecuteNonQuery();
+
+            //закрытие соединения с базой данных
+            m_dbConnection.Close();
+
         }
     }
 }
