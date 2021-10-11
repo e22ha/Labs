a8b4a8d66fb58dc9c22f627cfea012c0e785da86
Sergey <seregio2002@outlook.com>
2021-10-11 22:23:54 +0700

    another project

---
+++ hellow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hellow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
