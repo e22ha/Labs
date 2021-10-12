ea26f2c116eb233a1ed34ea7aabd99f9f81b428b
Sergey <seregio2002@outlook.com>
2021-10-12 11:20:36 +0700

    init

---
+++ CustomGallery/HoverButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomGallery
+{
+    class HoverButton : Button
+    {
+        public HoverButton() : base()
+        {
+            ForeColor = Color.White;
+            Font = new Font("Microsoft YaHei UI",
+             20.25F,
+             FontStyle.Bold,
+             GraphicsUnit.Point,
+             0);
+        }
+        private Color color = Color.SkyBlue;
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            // отрисовка прямоугольника
+            pe.Graphics.FillRectangle(new SolidBrush(color), ClientRectangle);
+            // отрисовка текста
+            pe.Graphics.DrawString(Text, Font, new SolidBrush(ForeColor), ClientRectangle);
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            color = Color.Blue;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            color = Color.SkyBlue;
+        }
+
+
+    }
+}
