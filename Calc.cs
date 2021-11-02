9b8af88a483a693e398b055829e1f48fb56465b2
Gleb <gleb.plaxienko@gmail.com>
2021-11-02 16:09:10 +0700

    Create a calculator

---
+++ Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNa5
+{
+    class Calc
+    {
+        public static double convr(int a)
+        {
+            return a / 2.54;
+        }
+
+        public static bool Even(int n)
+        {
+            return n % 2 == 0;
+        }
+
+        public static int max(int[] mas, int a)
+        {
+            a = mas.Max();
+            return a;
+        }
+
+        public static int mod(int a, int b)
+        {
+            if (b <= 0)
+            {
+                Console.WriteLine("Enter a value greater than 0");
+            }
+
+            return a % b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,58 @@ namespace TestNa5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0;
+            string op;
+
+            Console.WriteLine("\tConsole Calculator in C#\r");
+            Console.WriteLine("\t------------------------\n");
+
+            Console.WriteLine("\tSelect an operation:\n");
+            Console.WriteLine("\t1.Convert centimeters to inches\n");
+            Console.WriteLine("\t2.Checking a number for evenness\n");
+            Console.WriteLine("\t3.The largest value from array\n");
+            Console.WriteLine("\t4.Calculation of the remainder of division\n");
+            op = Console.ReadLine();
+
+            if (op == "1")
+            {
+                Console.WriteLine("\tEnter length in centimeters:\n");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Calc.convr(a) + " inches");
+            }
+            else if (op == "2")
+            {
+                Console.WriteLine("\tEnter the number:\n");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Calc.Even(a));
+            }
+            else if (op == "3")
+            {
+                Console.WriteLine("\tEnter length of array:\n");
+                int l = Convert.ToInt32(Console.ReadLine());
+                int[] mas = new int[l];
+
+                Console.WriteLine("\tEnter an array:\n");
+                for (int i = 0; i < l; i++)
+                {
+                    mas[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("\tThe highest number is ");
+                Console.WriteLine(Calc.max(mas, a));
+            }
+            else if (op == "4")
+            {
+                Console.WriteLine("\tEnter the number a:\n");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\tEnter the number b:\n");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\tThe remainder of dividing " + a + " by " + b);
+                Console.WriteLine(Calc.mod(a, b));
+            }
+
+            Console.ReadKey();
         }
     }
 }
