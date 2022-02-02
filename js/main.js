706402b44b23ead2ab62740f7cb500270730d5af
Gleb Plaxienko <gleb.plaxienko@gmail.com>
2022-02-02 14:40:58 +0700

    add launch

--- js/main.js
+++ js/main.js
@@ -4,52 +4,49 @@ var container;
 var camera, scene, renderer;
 
 // В этой функции можно добавлять объекты и выполнять их первичную настройку
-function init()
-{
- // Получение ссылки на элемент html страницы
- container = document.getElementById( 'container' );
- // Создание "сцены"
- scene = new THREE.Scene();
- // Установка параметров камеры
- // 45 - угол обзора
- // window.innerWidth / window.innerHeight - соотношение сторон
- // 1 - 4000 - ближняя и дальняя плоскости отсечения
- camera = new THREE.PerspectiveCamera(
- 45, window.innerWidth / window.innerHeight, 1, 4000 );
- // Установка позиции камеры
- camera.position.set(5, 5, 5);
+function init() {
+    // Получение ссылки на элемент html страницы
+    container = document.getElementById('container');
+    // Создание "сцены"
+    scene = new THREE.Scene();
+    // Установка параметров камеры
+    // 45 - угол обзора
+    // window.innerWidth / window.innerHeight - соотношение сторон
+    // 1 - 4000 - ближняя и дальняя плоскости отсечения
+    camera = new THREE.PerspectiveCamera(
+        45, window.innerWidth / window.innerHeight, 1, 4000);
+    // Установка позиции камеры
+    camera.position.set(5, 5, 5);
 
- // Установка точки, на которую камера будет смотреть
- camera.lookAt(new THREE.Vector3( 0, 0.0, 0));
- // Создание отрисовщика
- renderer = new THREE.WebGLRenderer( { antialias: false } );
- renderer.setSize( window.innerWidth, window.innerHeight );
- // Закрашивание экрана синим цветом, заданным в 16ричной системе
- renderer.setClearColor( 0x000000ff, 1);
- container.appendChild( renderer.domElement );
- // Добавление функции обработки события изменения размеров окна
- window.addEventListener( 'resize', onWindowResize, false );
+    // Установка точки, на которую камера будет смотреть
+    camera.lookAt(new THREE.Vector3(0, 0.0, 0));
+    // Создание отрисовщика
+    renderer = new THREE.WebGLRenderer({
+        antialias: false
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Закрашивание экрана синим цветом, заданным в 16ричной системе
+    renderer.setClearColor(0x000000ff, 1);
+    container.appendChild(renderer.domElement);
+    // Добавление функции обработки события изменения размеров окна
+    window.addEventListener('resize', onWindowResize, false);
 }
 
-function onWindowResize()
-{
- // Изменение соотношения сторон для виртуальной камеры
- camera.aspect = window.innerWidth / window.innerHeight;
- camera.updateProjectionMatrix();
- // Изменение соотношения сторон рендера
- renderer.setSize( window.innerWidth, window.innerHeight );
+function onWindowResize() {
+    // Изменение соотношения сторон для виртуальной камеры
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    // Изменение соотношения сторон рендера
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate()
-{
- // Добавление функции на вызов, при перерисовки браузером страницы
- requestAnimationFrame( animate );
- render();
-}
-function render()
-{
- // Рисование кадра
- renderer.render( scene, camera );
+function animate() {
+    // Добавление функции на вызов, при перерисовки браузером страницы
+    requestAnimationFrame(animate);
+    render();
 }
 
-
+function render() {
+    // Рисование кадра
+    renderer.render(scene, camera);
+}
