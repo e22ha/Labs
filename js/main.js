c83410e4e7bf920f6d6c6eb1a2d0d10a5784bcf8
Sergey Tychenko <58304169+SergeyTy@users.noreply.github.com>
2022-02-02 14:22:24 +0700

    setup min js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Ссылка на элемент веб страницы в котором будет отображаться графика
+var container;
+// Переменные "камера", "сцена" и "отрисовщик"
+var camera, scene, renderer;
+
+// В этой функции можно добавлять объекты и выполнять их первичную настройку
+function init()
+{
+ // Получение ссылки на элемент html страницы
+ container = document.getElementById( 'container' );
+ // Создание "сцены"
+ scene = new THREE.Scene();
+ // Установка параметров камеры
+ // 45 - угол обзора
+ // window.innerWidth / window.innerHeight - соотношение сторон
+ // 1 - 4000 - ближняя и дальняя плоскости отсечения
+ camera = new THREE.PerspectiveCamera(
+ 45, window.innerWidth / window.innerHeight, 1, 4000 );
+ // Установка позиции камеры
+ camera.position.set(5, 5, 5);
+
+ // Установка точки, на которую камера будет смотреть
+ camera.lookAt(new THREE.Vector3( 0, 0.0, 0));
+ // Создание отрисовщика
+ renderer = new THREE.WebGLRenderer( { antialias: false } );
+ renderer.setSize( window.innerWidth, window.innerHeight );
+ // Закрашивание экрана синим цветом, заданным в 16ричной системе
+ renderer.setClearColor( 0x000000ff, 1);
+ container.appendChild( renderer.domElement );
+ // Добавление функции обработки события изменения размеров окна
+ window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize()
+{
+ // Изменение соотношения сторон для виртуальной камеры
+ camera.aspect = window.innerWidth / window.innerHeight;
+ camera.updateProjectionMatrix();
+ // Изменение соотношения сторон рендера
+ renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate()
+{
+ // Добавление функции на вызов, при перерисовки браузером страницы
+ requestAnimationFrame( animate );
+ render();
+}
+function render()
+{
+ // Рисование кадра
+ renderer.render( scene, camera );
+}
+
+
