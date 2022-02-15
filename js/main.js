c9e6f9f44fa60388814f9b1ae5722bc1048c6d28
Sergey Tychenko <seregio2002@outlook.com>
2022-02-15 20:53:12 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//импорт библиотеки three.js
+import * as THREE from "./lib/three.module.js";
+//импорт библиотек для загрузки моделей и материалов
+import { MTLLoader } from './lib/MTLLoader.js';
+import { OBJLoader } from './lib/OBJLoader.js';
