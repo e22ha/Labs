83e76c604fdec8309fab6d64ffbeb607a87c8a6d
Sergey Tychenko <seregio2002@outlook.com>
2022-03-15 22:29:16 +0700

    add new map

--- js/main.js
+++ js/main.js
@@ -4,20 +4,25 @@ import * as THREE from "./lib/three.module.js";
 import { MTLLoader } from "./lib/MTLLoader.js";
 import { OBJLoader } from "./lib/OBJLoader.js";
 
-import {
-    GLTFLoader
-} from "./lib/GLTFLoader.js";
+import { GLTFLoader } from "./lib/GLTFLoader.js";
 
 // Ссылка на элемент веб страницы в котором будет отображаться графика
 var container;
 // Переменные "камера", "сцена" и "отрисовщик"
 var camera, scene, renderer;
 
-var N = 255;
+var N = 500;
 var a = 0.0;
 var imagedata;
 
 var clock = new THREE.Clock();
+
+var bird;
+var path;
+
+var geometry;
+
+var birds = [[], []];
 
 var mixer,
     morphs = [];
@@ -30,30 +35,20 @@ animate();
 
 // В этой функции можно добавлять объекты и выполнять их первичную настройку
 function init() {
-
     // Получение ссылки на элемент html страницы
     container = document.getElementById("container");
-    // Создание "сцены"
     scene = new THREE.Scene();
-    // Установка параметров камеры
-    // 45 - угол обзора
-    // window.innerWidth / window.innerHeight - соотношение сторон
-    // 1 - 4000 - ближняя и дальняя плоскости отсечения
+
     camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth / window.innerHeight,
         1,
         4000
     );
-    // Установка позиции камеры
-    camera.position.set(128, 255, 510);
-    
-    // Установка точки, на которую камера будет смотреть
-    camera.lookAt(new THREE.Vector3(128, 0.0, 128));
-    // Создание отрисовщика
-    renderer = new THREE.WebGLRenderer({
-        antialias: false
-    });
+    camera.position.set(N / 2, 250, N + 300);
+    camera.lookAt(new THREE.Vector3(N / 2, 0.0, 100));
+
+    renderer = new THREE.WebGLRenderer({ antialias: false });
     renderer.setSize(window.innerWidth, window.innerHeight);
     // Закрашивание экрана синим цветом, заданным в 16ричной системе
     renderer.setClearColor(0x995599ff, 1);
@@ -62,57 +57,41 @@ function init() {
     container.appendChild(renderer.domElement);
     // Добавление функции обработки события изменения размеров окна
     window.addEventListener("resize", onWindowResize, false);
-    
-    //создание точечного источника освещения, параметры: цвет, интенсивность, дальность
-    //создание точечного источника освещения заданного цвета
-    //создание точечного источника освещения, параметры: цвет, интенсивность, дальность
-    const light = new THREE.PointLight(0xffffff, 1, 1000);
-    light.position.set(300, 200, 128); //позиция источника освещения
-    light.castShadow = true; //включение расчёта теней от источника освещения
-    scene.add(light); //добавление источника освещения sв сцену
-    //настройка расчёта теней от источника освещения
-    light.shadow.mapSize.width = 1024; //ширина карты теней в пикселях
-    light.shadow.mapSize.height = 1024; //высота карты теней в пикселях
-    light.shadow.camera.near = 1; //расстояние, ближе которого не будет теней
-    light.shadow.camera.far = 3000; //расстояние, дальше которого не будет теней
 
-    const geometry = new THREE.PlaneGeometry(255, 255, 10, 10);
-    const material = new THREE.MeshLambertMaterial({
-        color: 0x008800,
-        side: THREE.DoubleSide,
-        wireframe: false,
-    });
-    
-    const plane = new THREE.Mesh(geometry, material);
-    
-    plane.position.x = 128;
-    plane.position.z = 128;
-    plane.rotation.x = Math.PI / 2;
-    scene.add(plane);
-    plane.receiveShadow = true;
-    // plane.castShadow = true;
-    
-    // plane.castShadow = true;
+    addLight();
 
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
     var img = new Image();
     img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
+        canvas.width = 500;
+        canvas.height = 500;
         context.drawImage(img, 0, 0);
         imagedata = context.getImageData(0, 0, img.width, img.height);
         // Пользовательская функция генерации ландшафта
         terrain();
-    }
+    };
     // Загрузка изображения с картой высот
-    img.src = 'img/plateau.jpg';
+    img.src = "img/plateau.jpg";
 
     // вызов функции загрузки модели (в функции Init)
     loadModel("models/", "Tree.obj", "Tree.mtl");
-    
+
     loadAnimatedModel("models/Parrot.glb");
     curveCreator();
+}
+
+function addLight() {
+    const light = new THREE.PointLight(0xffffff, 1, 10000);
+    light.position.set(10, 500, 250); //позиция источника освещения
+    light.castShadow = true; //включение расчёта теней от источника освещения
+    scene.add(light); //добавление источника освещения sв сцену
+
+    //настройка расчёта теней от источника освещения
+    light.shadow.mapSize.width = 1024; //ширина карты теней в пикселях
+    light.shadow.mapSize.height = 1024; //высота карты теней в пикселях
+    light.shadow.camera.near = 1; //расстояние, ближе которого не будет теней
+    light.shadow.camera.far = 3000;
 }
 
 function onWindowResize() {
@@ -123,21 +102,20 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function getPixel( imagedata, x, y )
-{
-    var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
-    return data[ position ];;
+function getPixel(imagedata, x, y) {
+    var position = (x + imagedata.width * y) * 4,
+        data = imagedata.data;
+    return data[position];
 }
 
 // В этой функции можно изменять параметры объектов и обрабатывать действия пользователя
 function animate() {
     // воспроизведение анимаций (в функции animate)
     var delta = clock.getDelta();
-    mixer.update(delta);
-    console.log(mixer);
-    for (var i = 0; i < morphs.length; i++) {
-        var morph = morphs[i];
-    }
+    // mixer.update(delta);
+    // for (var i = 0; i < morphs.length; i++) {
+    //     var morph = morphs[i];
+    // }
 
     // Добавление функции на вызов, при перерисовки браузером страницы
     requestAnimationFrame(animate);
@@ -178,10 +156,21 @@ function loadModel(path, oname, mname) {
                         });
 
                         for (var i = 0; i < 4; i++) {
-                            object.position.x = 1 + Math.random() * 200;
-                            object.position.z = 1 + Math.random() * 200;
+                            var X = Math.random() * N;
+                            var Z = Math.random() * N;
+                            object.position.x = X;
+                            object.position.z = Z;
+                            // var v = geometry.getAttribute("position");
+                            // var Y =
+                            //     v[
+                            //         Math.round(x) + math.round(z) * N
+                            //     ].y;
+                            object.position.y = 0;
 
-                            object.scale.set(0.5, 0.5, 0.5);
+                            object.rotation.y = Math.PI * 8;
+                            var s = Math.random() * 100 + 100;
+                            s /= N;
+                            object.scale.set(s, s, s);
 
                             scene.add(object.clone());
                         }
@@ -193,11 +182,11 @@ function loadModel(path, oname, mname) {
 }
 
 function terrain() {
+    geometry = new THREE.BufferGeometry(); // Создание структуры для хранения геометрии
     var vertices = []; // Объявление массива для хранения вершин
     var faces = []; // Объявление массива для хранения индексов
-    var uvs = []; // Массив для хранения текстурных координат 
+    var uvs = []; // Массив для хранения текстурных координат
 
-    var geometry = new THREE.BufferGeometry(); // Создание структуры для хранения геометрии
 
     for (var i = 0; i < N; i++)
         for (var j = 0; j < N; j++) {
@@ -205,31 +194,34 @@ function terrain() {
             var h = getPixel(imagedata, i, j);
 
             vertices.push(i, h / 5, j); // Добавление координат третьей вершины в массив вершин
-            uvs.push(i / (N - 1), j / (N - 1)); // Добавление цвета для первой вершины (красный)  
+            uvs.push(i / (N - 1), j / (N - 1)); // Добавление цвета для первой вершины (красный)
         }
 
     for (var i = 0; i < N - 1; i++)
         for (var j = 0; j < N - 1; j++) {
-            faces.push(i + j * N, (i + 1) + j * N, (i + 1) + (j + 1) * N); // Добавление индексов (порядок соединения вершин) в массив индексов
-            faces.push(i + j * N, (i + 1) + (j + 1) * N, i + (j + 1) * N); // Добавление индексов (порядок соединения вершин) в массив индексов
+            faces.push(i + j * N, i + 1 + j * N, i + 1 + (j + 1) * N); // Добавление индексов (порядок соединения вершин) в массив индексов
+            faces.push(i + j * N, i + 1 + (j + 1) * N, i + (j + 1) * N); // Добавление индексов (порядок соединения вершин) в массив индексов
         }
 
     //Добавление вершин и индексов в геометрию
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(vertices, 3)
+    );
     geometry.setIndex(faces);
 
     //Добавление текстурных координат в геометрию
-    geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
+    geometry.setAttribute("uv", new THREE.Float32BufferAttribute(uvs, 2));
 
     geometry.computeVertexNormals();
 
     // Загрузка текстуры yachik.jpg из папки pics
-    var tex = new THREE.TextureLoader().load('img/grasstile.jpg');
+    var tex = new THREE.TextureLoader().load("img/grasstile.jpg");
 
     var material = new THREE.MeshLambertMaterial({
         map: tex,
         wireframe: false,
-        side: THREE.DoubleSide
+        side: THREE.DoubleSide,
     });
 
     // Режим повторения текстуры
@@ -254,15 +246,15 @@ function loadAnimatedModel(path) {
         var clip = gltf.animations[0];
         //установка параметров анимации (скорость воспроизведения и стартовый фрейм)
 
-
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < 1; i++) {
             mesh.position.set(
-                1 + Math.random() * 200,
-                20,
-                1 + Math.random() * 200
+                1 + Math.random() * N,
+                100,
+                1 + Math.random() * N
             );
             mesh.rotation.y = Math.PI / 8;
-            mesh.scale.set(0.5, 0.5, 0.5);
+            var s = Math.random() * 100 + 30;
+            mesh.scale.set(0.3, 0.3, 0.3);
 
             mesh.castShadow = true;
 
@@ -270,6 +262,8 @@ function loadAnimatedModel(path) {
 
             scene.add(clone);
             morphs.push(clone);
+            birds[i] = clone;
+            console.log(birds[i][i]);
             mixer.clipAction(clip, clone).setDuration(1).startAt(0).play();
         }
     });
@@ -277,19 +271,18 @@ function loadAnimatedModel(path) {
 
 function curveCreator() {
     var curve1 = new THREE.CubicBezierCurve3(
-        new THREE.Vector3(50, 0, 128), //P0
-        new THREE.Vector3(50, 0, 0), //P1
-        new THREE.Vector3(200, 0, 0), //P2
-        new THREE.Vector3(200, 0, 128) //P3
+        new THREE.Vector3(100, 100, 256), //P0
+        new THREE.Vector3(100, 80, 0), //P1
+        new THREE.Vector3(400, 110, 0), //P2
+        new THREE.Vector3(400, 100, 256) //P3
     );
     var vertices = [];
     var curve2 = new THREE.CubicBezierCurve3(
-        new THREE.Vector3(50, 0, 128), //P0
-        new THREE.Vector3(50, 0, 256), //P1
-        new THREE.Vector3(200, 0, 256), //P2
-        new THREE.Vector3(200, 0, 128) //P3
+        new THREE.Vector3(400, 100, 256), //P3
+        new THREE.Vector3(400, 100, 512), //P2
+        new THREE.Vector3(100, 100, 512), //P1
+        new THREE.Vector3(100, 100, 256) //P0
     );
-
 
     vertices = curve1.getPoints(20);
     vertices = vertices.concat(curve2.getPoints(20));
@@ -306,13 +299,14 @@ function curveCreator() {
     //создание объекта
     var curveObject = new THREE.Line(geometry, material);
     scene.add(curveObject); //добавление объекта в сцену
+    return curveObject;
 }
 
-function moveByPath(){
+function moveByPath(object, path) {
     //Для нахождения такой точки может быть использован метод кривой getPointAt:
-    var pos = new THREE.Vector3();
-    pos.copy(path.getPointAt(t / T));
-    var nextPoint = new THREE.Vector3();
-    nextPoint.copy(path.getPointAt((t + 0.1) / T));
-    object.lookAt(nextPoint);
+    // var pos = new THREE.Vector3();
+    // pos.copy(path.getPointAt(1 / 10));
+    // var nextPoint = new THREE.Vector3();
+    // nextPoint.copy(path.getPointAt((1 + 0.1) / 10));
+    // object.lookAt(nextPoint);
 }
