b099b799583ff22c9a0abbc4b3bb121b6cb4c2b8
Sergey Manzhos <sergey.manzhos@outlook.com>
2023-08-23 14:35:44 +0700

    merge js_GLSL

--- PTG/js_GLSL/js/main.js
+++ PTG/js_GLSL/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //импорт библиотеки three.js
 // import * as THREE from "";
 import * as THREE from "three";
@@ -31,10 +32,13 @@ let ListModel = [
 
 let loadedModels = new Map();
 
+=======
+>>>>>>> js_GLSL/master
 // Ссылка на элемент веб страницы в котором будет отображаться графика
 var container;
 // Переменные "камера", "сцена" и "отрисовщик"
 var camera, scene, renderer;
+<<<<<<< HEAD
 
 var cameraOrtho, sceneOrtho;
 
@@ -84,11 +88,21 @@ var wind = new THREE.Vector3(0.0, 0.0, 0.0);
 // Функция инициализации камеры, отрисовщика, объектов сцены и т.д.
 init();
 // Обновление данных по таймеру браузера
+=======
+var sphere;
+var clock = new THREE.Clock();
+
+// Функция инициализации камеры, отрисовщика, объектов сцены и т.д.
+init();
+// Обновление данных по таймеру браузера
+animate();
+>>>>>>> js_GLSL/master
 
 // В этой функции можно добавлять объекты и выполнять их первичную настройку
 function init() {
     // Получение ссылки на элемент html страницы
     container = document.getElementById("container");
+<<<<<<< HEAD
 
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild(stats.dom);
@@ -96,6 +110,10 @@ function init() {
     scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
+=======
+    // Создание "сцены"
+    scene = new THREE.Scene();
+>>>>>>> js_GLSL/master
     // Установка параметров камеры
     // 45 - угол обзора
     // window.innerWidth / window.innerHeight - соотношение сторон
@@ -104,6 +122,7 @@ function init() {
         45,
         window.innerWidth / window.innerHeight,
         1,
+<<<<<<< HEAD
         600
     );
 
@@ -555,10 +574,134 @@ function onWindowResize() {
 
     // Изменение соотношения сторон рендера
     renderer.setSize(width, height);
+=======
+        4000
+    );
+    // Установка позиции камеры
+    camera.position.set(-100, 0, 0);
+
+    // Установка точки, на которую камера будет смотреть
+    camera.lookAt(new THREE.Vector3(0, 0.0, 0));
+    // Создание отрисовщика
+    renderer = new THREE.WebGLRenderer({
+        antialias: false,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Закрашивание экрана синим цветом, заданным в 16ричной системе
+    renderer.setClearColor(0x000000ff, 1);
+    container.appendChild(renderer.domElement);
+    // Добавление функции обработки события изменения размеров окна
+    window.addEventListener(
+        "resize",
+        onWindowResize,
+        false
+    );
+
+    //add sphere
+    var geometry = new THREE.SphereGeometry(32, 64, 64);
+    // var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    // var sphere = new THREE.Mesh(geometry, material);
+    // scene.add(sphere);
+    //создание списка смещений для вершин
+    //создание списка смещений для вершин
+    var displacement = new Float32Array(
+        geometry.attributes.position.array.length
+    );
+    for (
+        var i = 0;
+        i < geometry.attributes.position.array.length;
+        i++
+    ) {
+        var rand = Math.random() * 2 - 1; //случайное значение в диапазоне от -1 до 1
+        displacement[i] = rand;
+    }
+    
+    geometry.computeTangents();
+    
+    for (
+        var i = 0;
+        i < geometry.attributes.tangent.array.length;
+        i++
+    ) {
+        geometry.attributes.tangent.array[i] =
+            -geometry.attributes.tangent.array[i];
+    }
+
+    //установка списка смещений в качестве атрибута геометрии
+    geometry.setAttribute(
+        "displacement",
+        new THREE.BufferAttribute(displacement, 1)
+    );
+
+    //загрузка текстуры
+    var earthTex = new THREE.TextureLoader().load(
+        "img/earth_atmos_2048.jpg"
+    );
+    var earthTex_ = new THREE.TextureLoader().load(
+        "img/earth_lights_2048.png"
+    );
+
+    var earthTex_norm = new THREE.TextureLoader().load(
+        "img/earth_normal_2048.jpg"
+    );
+
+    var shaderMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            dTex: { value: earthTex }, //текстура
+            nTex: { value: earthTex_ },
+            normTex: { value: earthTex_norm }, //текстура
+            lightPosition: { value: new THREE.Vector3(0.0, 0.0, 10000.0) },            
+            color: {
+                value: new THREE.Vector4(
+                    1.0,
+                    1.0,
+                    1.0,
+                    1.0
+                ),
+            },
+            ambientColor: {
+                value: new THREE.Vector4(
+                    0.5,
+                    0.5,
+                    0.5,
+                    1.0
+                ),
+            },
+            lightColor: {
+                value: new THREE.Vector4(
+                    1.0,
+                    1.0,
+                    1.0,
+                    1.0
+                ),
+            },
+            //,
+            //amp: { value: 1.0 }, //аплитуда
+        },
+        vertexShader:
+            document.getElementById("vertexShader")
+                .textContent,
+        fragmentShader: document.getElementById(
+            "fragmentShader"
+        ).textContent,
+    });
+
+    sphere = new THREE.Mesh(geometry, shaderMaterial);
+    scene.add(sphere);
+}
+
+function onWindowResize() {
+    // Изменение соотношения сторон для виртуальной камеры
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    // Изменение соотношения сторон рендера
+    renderer.setSize(window.innerWidth, window.innerHeight);
+>>>>>>> js_GLSL/master
 }
 
 // В этой функции можно изменять параметры объектов и обрабатывать действия пользователя
 function animate() {
+<<<<<<< HEAD
 
     // Добавление функции на вызов, при перерисовки браузером страницы
     stats.begin();
@@ -1293,4 +1436,16 @@ function buttonHover(mouseX, mouseY, spr) {
             return true;
         }
     return false;
+=======
+    var time = clock.getDelta();
+    sphere.rotation.y += (Math.PI /4*time);
+    //sphere.material.uniforms.amp.value = Math.cos(time) * 5;
+    // Добавление функции на вызов, при перерисовки браузером страницы
+    requestAnimationFrame(animate);
+    render();
+}
+function render() {
+    // Рисование кадра
+    renderer.render(scene, camera);
+>>>>>>> js_GLSL/master
 }
